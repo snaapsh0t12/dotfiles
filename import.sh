8d8a45d6ba48b1c8ebaafc103cc5f2ba0b3b1a8c
Aiden <aidenjlee9478@gmail.com>
2023-03-28 10:49:02 -0500

    Added enough to make the import.sh work

---
+++ import.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILES=$(sed -r 's/^\s*(.*\S)*\s*$/\1/;/^$/d' files.txt)
+
+echo "$FILES" | while read -r line ; do
+    # echo "$line"
+    mkdir -p "./$(dirname $line)"
+    cp -r "$HOME/$line" ./$line
+done
