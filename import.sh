81d25b2a65d62862092e1dcf9ac8d758727195e1
Aiden <aidenjlee9478@gmail.com>
2023-04-07 16:26:33 -0500

    Added neovim files and excluded .github in import

--- import.sh
+++ import.sh
@@ -5,5 +5,5 @@ FILES=$(sed -r 's/^\s*(.*\S)*\s*$/\1/;/^$/d' files.txt)
 echo "$FILES" | while read -r line ; do
     echo "Copying $line..."
     mkdir -p "./$(dirname $line)"
-    rsync -r --exclude '.git' "$HOME/$line" ./$line
+    rsync -r --exclude '.git' --exclude '.github' "$HOME/$line" ./$line
 done
